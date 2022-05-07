b8747dc8db656a670f85a1bad60f4d275af4c370
liubin_SWP <liubin@qq.com>
2022-05-07 14:48:50 +0800

    3-5 修改成开发环境的数据库wikidev

--- doc/all.sql
+++ doc/all.sql
@@ -1,6 +1,7 @@
 create table `test`(
                        `id` bigint not null comment 'id',
                        `name` varchar(50) comment '名称',
+                       `password` varchar(50) comment '密码',
                        primary key (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment ='测试';
 select * from test;
