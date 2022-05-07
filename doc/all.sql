a4eb89f061e09c734cfda3aed0a0837b765cb97f
liubin_SWP <liubin@qq.com>
2022-05-07 15:12:35 +0800

    3-6 继承mybatis generater 生成demo 表代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,9 @@ create table `test`(
                        primary key (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment ='测试';
 select * from test;
+
+create table `demo`(
+                       `id` bigint not null comment 'id',
+                       `name` varchar(50) comment '名称',
+                       primary key (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment ='测试';
