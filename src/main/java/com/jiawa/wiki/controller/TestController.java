b693dec5478d25b1f0d7fcc3a86102b9ba9f324d
liubin_SWP <liubin@qq.com>
2022-05-07 12:18:27 +0800

    2-7 配置文件为 类添加值

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:默认值}")
+    private String testHello;
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 }
