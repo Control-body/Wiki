82904ff0816c656a604d49d0a382925529b95886
liubin_SWP <liubin@qq.com>
2022-05-07 15:59:11 +0800

    3-8 返回前端统一接口

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.service.EbookServce;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,10 @@ public class EbookController {
     @Resource
     private EbookServce ebookServce;
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookServce.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> objectCommonResp = new CommonResp<>();
+        List<Ebook> list = ebookServce.list();
+        objectCommonResp.setContent(list);
+        return objectCommonResp;
     }
 }
