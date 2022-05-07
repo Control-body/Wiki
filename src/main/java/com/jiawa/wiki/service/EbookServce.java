284b67dd22a0add62118740a7570f72a97fd1400
liubin_SWP <liubin@qq.com>
2022-05-07 23:07:16 +0800

    4-2 service层增加 动态sql

--- src/main/java/com/jiawa/wiki/service/EbookServce.java
+++ src/main/java/com/jiawa/wiki/service/EbookServce.java
@@ -7,6 +7,7 @@ import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,15 +21,10 @@ public class EbookServce {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
+        if(!ObjectUtils.isEmpty(req.getName()))
         criteria.andNameLike("%"+ req.getName()+"%%"); // 模糊匹配查询条件
+
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-//        List<EbookResp> respList=new ArrayList<>();
-//        for(Ebook ebook: ebookList){
-//            EbookResp ebookResp = new EbookResp();
-//            BeanUtils.copyProperties(ebook,ebookResp);
-//         //   ebookResp.setId(123L);
-//            respList.add(ebookResp);
-//        }
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);// 后面是泛型 里面的类
         return respList;
     }
