ecc884bb7f79bcca576e200f800cb47e8487891d
liubin_SWP <liubin@qq.com>
2022-05-07 21:35:18 +0800

    4- 添加跨域访问的 配置类

--- src/main/java/com/jiawa/wiki/service/EbookServce.java
+++ src/main/java/com/jiawa/wiki/service/EbookServce.java
@@ -5,11 +5,10 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.jiawa.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,14 +22,14 @@ public class EbookServce {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+ req.getName()+"%%"); // 模糊匹配查询条件
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList=new ArrayList<>();
-        for(Ebook ebook: ebookList){
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-         //   ebookResp.setId(123L);
-            respList.add(ebookResp);
-
-        }
+//        List<EbookResp> respList=new ArrayList<>();
+//        for(Ebook ebook: ebookList){
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook,ebookResp);
+//         //   ebookResp.setId(123L);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);// 后面是泛型 里面的类
         return respList;
     }
 }
