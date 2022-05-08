cd150c99634439155924d17727750756b3f63285
liubin_SWP <liubin@qq.com>
2022-05-08 13:00:55 +0800

    6.1 分页插件的使用

--- src/main/java/com/jiawa/wiki/service/EbookServce.java
+++ src/main/java/com/jiawa/wiki/service/EbookServce.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookServce {
+    private static final Logger Log= LoggerFactory.getLogger(EbookServce.class);
+
     @Resource
     //JDK自带的
     //@Autowired // 自动注入 Spring带的
@@ -23,8 +29,11 @@ public class EbookServce {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName()))
         criteria.andNameLike("%"+ req.getName()+"%%"); // 模糊匹配查询条件
-
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        Log.info("总行数:{}",pageInfo.getTotal());
+        Log.info("总页数:{}",pageInfo.getPages());
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);// 后面是泛型 里面的类
         return respList;
     }
